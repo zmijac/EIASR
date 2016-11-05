2abdfa07741943b8e51ee86e2c3fb594be1e9a8e
Antun Aleksa <antun.lobotomija@gmail.com>
2016-11-05 19:55:12 +0100

    Main with the back projection algorithm.

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
-# EIASR PALM TRACKING PROJECT
+"""Project for EIASR class. WUT, year 2016/2017"""
 
 import numpy as np
+import argparse
 import cv2
 
-def skin_color(pixel):
-    
-    Y_min=88
-    Y_max=169
-    U_min=110
-    U_max=114
-    V_min=146
-    V_max=151
-    
-    #if pixel[0]>Y_min and pixel[0]<Y_max:
-    if pixel[1]>V_min and pixel[1]<V_max:
-        if pixel[2]>U_min and pixel[2]<U_max:
-            pixel=[241, 128, 128]
-            return pixel
-    pixel=[49, 128, 128]
-    return pixel
+"""
+Runs the backprojection algorithm. 
+The output is a image showing the detected object in black and white and also with applied color histagram.
 
-img = cv2.imread('hand.png')
-width, height, depth = img.shape
-output_img = np.zeros((width, height, 3), np.uint8)
+target - target image
+"""
+def backprojection(target):
 
-output_imgYVU = cv2.cvtColor(output_img, cv2.COLOR_BGR2YCR_CB)
-imgYVU = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    hsvt = cv2.cvtColor(target,cv2.COLOR_BGR2HSV)
 
-for i in range(0, width):
-	for j in range(0, height):
-		pixel = imgYVU[i, j]
-		output_imgYVU[i, j] = skin_color(pixel)
+    height, width, depth = target.shape
 
-output_img = cv2.cvtColor(output_imgYVU, cv2.COLOR_YCR_CB2BGR)
+    # finding the region of interest
+    roi = target[height / 2 - 40 : height / 2 + 40, width / 2 - 40 : width / 2 + 40]
+    hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
 
-cv2.imshow('hand', output_img)
-cv2.waitKey(0)
+    # debug
+    #cv2.imshow('hand', roi)
+    #cv2.waitKey(0)
+
+    # calculating object histogram
+    roihist = cv2.calcHist([hsv],[0, 1], None, [180, 256], [0, 180, 0, 256] )
+
+    # normalize histogram and apply backprojection
+    cv2.normalize(roihist,roihist,0,255,cv2.NORM_MINMAX)
+    dst = cv2.calcBackProject([hsvt],[0,1],roihist,[0,180,0,256],1)
+
+    # Now convolute with circular disc
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    cv2.filter2D(dst,-1,disc,dst)
+
+    # threshold and binary AND
+    ret,thresh = cv2.threshold(dst,50,255,0)
+    thresh = cv2.merge((thresh,thresh,thresh))
+    res = cv2.bitwise_and(target,thresh)
+
+    res = np.vstack((target,thresh,res))
+    cv2.imwrite('res.jpg',res)
+
+
+def main():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", help = "path to the image")
+    args = vars(ap.parse_args())
+
+    # load the image
+    image = cv2.imread(args["image"])
+
+    backprojection(image)
+
+
+if __name__ == "__main__":
+    main()
